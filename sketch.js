8088ae5a17e653322cb622e45968c1f6dbc55f5c
NielsPichon <niels.pichon@outlook.com>
2021-08-23 23:48:37 +0200

    Started implementing the GUI. Struggling with css

---
+++ sketch.js
@@ -0,0 +1,6 @@
+function setup() {
+    let canvas = createCanvas(594, 840);
+    // Move the canvas so it’s inside our <div id="sketch-holder">.
+    canvas.parent('sketch-holder');
+    background(0, 0, 0);
+  }
